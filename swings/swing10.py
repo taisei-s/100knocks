2232d52a4406681e36a7e6e33481cf2af037a70d
taisei-s <suger60.t@gmail.com>
2019-02-28 17:43:45 +0900

    change return -> print

--- swings/swing10.py
+++ swings/swing10.py
@@ -15,8 +15,8 @@ def swing10():
     with open(filename, 'r') as f:
         lines = f.readlines()
 
-    return len(lines)
+    print(len(lines))
 
 
 if __name__ == "__main__":
-    print(swing10())
+    swing10()
